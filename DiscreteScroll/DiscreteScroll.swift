9ac68aa7df24f555947e7ee58cb9efd54dc7784f
quesera2 <quesera2262@gmail.com>
2026-07-05 17:57:11 +0900

    add access modifier

--- DiscreteScroll/DiscreteScroll.swift
+++ DiscreteScroll/DiscreteScroll.swift
@@ -10,13 +10,13 @@ struct TapContext {
 @MainActor
 struct DiscreteScroll {
     
-    static let accessibilityNotificationName: Notification.Name = Notification.Name("com.apple.accessibility.api")
+    private static let accessibilityNotificationName: Notification.Name = Notification.Name("com.apple.accessibility.api")
     
-    static let defaultLineCount: Int64 = 3
+    private static let defaultLineCount: Int64 = 3
     
-    static let keyLineCount = "lines"
+    private static let keyLineCount = "lines"
     
-    static let tapCallback: CGEventTapCallBack = { _, type, event, userInfo in
+    private static let tapCallback: CGEventTapCallBack = { _, type, event, userInfo in
         guard
             let userInfo = userInfo?.load(as: TapContext.self)
         else {
@@ -47,7 +47,7 @@ struct DiscreteScroll {
     }
     
     /// アクセシビリティ許可をチェックし、許可済みとなるまで待機する
-    func waitUntilTrusted() async {
+    private func waitUntilTrusted() async {
         if AXIsProcessTrusted() { return }
         
         // kAXTrustedCheckOptionPrompt の定数を直接参照する
@@ -62,7 +62,7 @@ struct DiscreteScroll {
     }
     
     /// スクロール状態の監視のコールバックを設定する
-    func setupObserveScroll() {
+    private func setupObserveScroll() {
         // スクロール行数と作成したCFMachPortをポインタに詰める、これはアプリが常駐している間、意図的に解放しない
         let linesValue = self.lines ?? DiscreteScroll.defaultLineCount
         let pointer = UnsafeMutablePointer<TapContext>.allocate(capacity: 1)
@@ -82,7 +82,7 @@ struct DiscreteScroll {
     }
     
     /// エラーダイアログを表示して終了
-    func displayNoticeAndExit(_ alertHeader: String) -> Never {
+    private func displayNoticeAndExit(_ alertHeader: String) -> Never {
         CFUserNotificationDisplayNotice(0,
                                         kCFUserNotificationCautionAlertLevel,
                                         nil,
